4c6222bb15792f9ab5404452272592eabb7c0e02
Kevin Cathaly <alakme@gmail.com>
2017-01-30 18:17:00 +0100

    Add Carthage support

---
+++ Example/Pods/Amplitude-iOS-Carthage/Amplitude_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Amplitude-iOS-Carthage.h
+//  Amplitude-iOS-Carthage
+//
+//  Created by Kevin CATHALY on 30/01/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Amplitude-iOS-Carthage.
+FOUNDATION_EXPORT double Amplitude_iOSVersionNumber;
+
+//! Project version string for Amplitude-iOS-Carthage.
+FOUNDATION_EXPORT const unsigned char Amplitude_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amplitude_iOS_Carthage/PublicHeader.h>
+
+
---
+++ Example/Pods/Segment-Amplitude-Carthage/Segment_Amplitude.h
@@ -0,0 +1,19 @@
+//
+//  Segment-Amplitude-Carthage.h
+//  Segment-Amplitude-Carthage
+//
+//  Created by Kevin CATHALY on 30/01/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Segment-Amplitude-Carthage.
+FOUNDATION_EXPORT double Segment_AmplitudeVersionNumber;
+
+//! Project version string for Segment-Amplitude-Carthage.
+FOUNDATION_EXPORT const unsigned char Segment_AmplitudeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segment_Amplitude_Carthage/PublicHeader.h>
+
+
